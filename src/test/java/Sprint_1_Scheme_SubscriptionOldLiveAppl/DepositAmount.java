6a20b75fda1da6d58a570fb9fbf670f9708e31ec
admin <admin@DESKTOP-B5HII1M>
2023-10-13 12:04:42 +0530

    com

--- src/test/java/Sprint_1_Scheme_SubscriptionOldLiveAppl/DepositAmount.java
+++ src/test/java/Sprint_1_Scheme_SubscriptionOldLiveAppl/DepositAmount.java
@@ -62,58 +62,20 @@ public class DepositAmount {
 		driver.get(URL);
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@placeholder='Email / mobile number']")).sendKeys(USERNAME,Keys.ENTER);
+		driver.findElement(By.xpath("//span[.='Booking Account']")).click();
+		String BeforeAddingAmountToBookingAccount = driver.findElement(By.xpath("//p[contains(.,'₹ ')]")).getText();
+		driver.findElement(By.xpath("//input[@placeholder='Enter amount']")).sendKeys("100000");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//div[.='Netbanking']")).click();
+		driver.findElement(By.xpath("//div[.='HDFC']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
+		driver.findElement(By.xpath("//button[.='Pay Now']")).click();
 		
-		LandingPage lPage = new LandingPage(driver);
-		System.out.println("Before adding amount to booking account");
-		lPage.clickOnBookingAccountLnk(driver);
-        
-//		lPage.viewBookingAccountBalance(driver);
-		
-		BookingAccountPage bap = new BookingAccountPage(driver);
-		bap.depositMoneyInBookingAccount(driver);
-		
-		WebElement element = driver.findElement(By.xpath("//iframe[contains(@style,'op')]"));
-		driver.switchTo().frame(element);
-		
-		BookingAccountPhNoFramePage bapnofp = new BookingAccountPhNoFramePage(driver);
-		bapnofp.enterPhoneNumberAndYesBankBtnClickOnProceed(driver);
-		
-		String parentWindow = driver.getWindowHandle();
-		driver.findElement(By.xpath("//div[@class='redesign-v15-cta-wrapper svelte-s8db8t']")).click();
-		
-		BookingAccountPhNoFramePage b = new BookingAccountPhNoFramePage(driver);
-		
-		Set<String> childWindow = driver.getWindowHandles();
-				
-		for(String childids : childWindow)
-		{
-			if(!childids.equals(parentWindow))
-			{
-				driver.switchTo().window(childids);
-				driver.findElement(By.xpath("//button[.='Success']")).click();
-			}
-		}
-		driver.switchTo().window(parentWindow);
-		
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
-                
-        wait.until(ExpectedConditions.alertIsPresent());
-        
-		String AlertMsg = driver.switchTo().alert().getText();
-		System.out.println(AlertMsg);
-		
-		if(AlertMsg.contains("Successfully added Rs"))
-		{
-			System.out.println("Amount Added Successfully From Yes Bank");
-		}
-		else
-		{
-			System.out.println("Amount Not Added Successfully");
-		}
-		driver.switchTo().alert().accept();
-		
-
-		System.out.println("After adding amount to booking account");
-		lPage.clickOnBookingAccountLnk(driver);
 	}
 }
